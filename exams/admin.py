f43500d95bd703c540aa4e12d3c51101f4e9c474
Amir Keivan Mohtashami <>
2015-01-10 02:03:25 +0330

    Removed django_jalali from imports

--- exams/admin.py
+++ exams/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from exams.models import Exam
 from exams.models import Question, Choice, MadeChoice
-import django_jalali.admin as jadmin
 
 # Register your models here.
 admin.site.register(Exam)
