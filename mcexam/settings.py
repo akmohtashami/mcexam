2a66bd9072215831c5d46bb5b18dbf88dfe403c8
Amir Keivan Mohtashami <>
2015-01-10 01:54:34 +0330

    Added heroku settings

--- mcexam/settings.py
+++ mcexam/settings.py
@@ -131,3 +131,16 @@ import dj_database_url
 DATABASES["default"] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
