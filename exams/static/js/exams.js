4faab25219b2eaaaea19266f9f32942d298b6c3c
Amir Keivan Mohtashami <>
2015-02-03 22:44:11 +0330

    Fixed exams.js

--- exams/static/js/exams.js
+++ exams/static/js/exams.js
@@ -1,18 +1,24 @@
-$('.choice').on("click", function(event){
-    var radio_str = "[data-identifier='" + $(this).data("correspond") + "']";
-    var obj = $(document).find(radio_str);
-    var currently_selected = $("input[name='" + $(obj).prop("name") + "']:checked");
-    if (currently_selected.length && currently_selected != obj) {
-        var selected_label = $(document).find("[data-correspond='" + $(currently_selected).data("identifier") + "']");
-        selected_label.removeClass("marked");
-    }
-    var current = $(obj).prop("checked");
-    $(obj).prop("checked", !current);
-    if (!current)
-        $(this).addClass("marked");
-    else
-        $(this).removeClass("marked");
+$(document).ready(function() {
+    $('.choice').on("click", function (event) {
+        var radio_str = "[data-identifier='" + $(this).data("correspond") + "']";
+        var obj = $(document).find(radio_str);
+        var currently_selected = $("input[name='" + $(obj).prop("name") + "']:checked");
+        if (currently_selected.length && currently_selected != obj) {
+            var selected_label = $(document).find("[data-correspond='" + $(currently_selected).data("identifier") + "']");
+            selected_label.removeClass("marked");
+        }
+        var current = $(obj).prop("checked");
+        $(obj).prop("checked", !current);
+        if (!current)
+            $(this).addClass("marked");
+        else
+            $(this).removeClass("marked");
+    });
+    $("[data-delete=true]").click(function(e) {
+        return confirm(gettext("Please confirm to continue"));
+    });
 });
+
 
 /*
 $("[data-delete=true]").click(function(e){
@@ -36,6 +42,3 @@ $("[data-delete=true]").click(function(e){
         });
 });
 */
-$("[data-delete=true]").click(function(e){
-    return confirm(gettext("Please confirm to continue"));
-});
