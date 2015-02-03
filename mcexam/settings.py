e24aecdd16a31d8ad6f2c5db821e4fe034a5a070
Amir Keivan Mohtashami <>
2015-02-03 22:46:23 +0330

    Fixed exams.js

--- mcexam/settings.py
+++ mcexam/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_cleanup',
-    'rosetta',
     'resources',
     'guardian',
     "adminsortable",
@@ -175,7 +174,7 @@ LOCALE_PATHS = (
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
